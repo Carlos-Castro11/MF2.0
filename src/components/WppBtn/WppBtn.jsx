d913afe46369aa881573f4eb594108fac8a28d8a
Your Name <carloshenriquemg2020@gmail.com>
2023-11-05 18:51:28 -0300

    96

--- src/components/WppBtn/WppBtn.jsx
+++ src/components/WppBtn/WppBtn.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import styles from "./WppBtn.module.css";
 
-import wpp from "../../assets/icons/whatsapp.svg";
+import wpp from "../../assets/icons/whatsappBtn.svg";
 
 const WppBtn = () => {
+  const [teste, setTeste] = React.useState(true);
+
+  function handleScroll() {
+    console.log(window.innerHeight);
+    if (window.innerHeight > 663) {
+      setTeste(false);
+    }
+  }
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <a className={styles.wppBtn} href="google.com">
-      <img src={wpp} alt="" />
-    </a>
+    <div>
+      {teste ? (
+        <a className={styles.wppBtn} href="google.com">
+          <img src={wpp} alt="" />
+        </a>
+      ) : null}
+    </div>
   );
 };
 

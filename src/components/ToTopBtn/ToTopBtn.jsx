2249947cbe8f0449f280c645ce12c34610eb89ff
Your Name <carloshenriquemg2020@gmail.com>
2023-11-05 18:30:37 -0300

    nf

--- src/components/ToTopBtn/ToTopBtn.jsx
+++ src/components/ToTopBtn/ToTopBtn.jsx
@@ -4,16 +4,6 @@ import styles from "./ToTopBtn.module.css";
 import toTopBtn from "../../assets/icons/backToTopButton.png";
 
 const ToTopBtn = () => {
-  function handleHeight() {
-    if (window.innerHeight > 2000) {
-      console.log("opa");
-    }
-  }
-
-  React.useEffect(() => {
-    window.addEventListener("scroll", handleHeight);
-  }, []);
-
   return (
     <a className={styles.toTopBtn} href="google.com">
       <img src={toTopBtn} alt="" />

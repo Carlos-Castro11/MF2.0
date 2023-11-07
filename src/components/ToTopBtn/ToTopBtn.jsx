2531e5a444ef099f531baead9970d179c8b1ec3e
Your Name <carloshenriquemg2020@gmail.com>
2023-11-07 17:41:40 -0300

    95%

--- src/components/ToTopBtn/ToTopBtn.jsx
+++ src/components/ToTopBtn/ToTopBtn.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./ToTopBtn.module.css";
 
-import toTopBtn from "../../assets/icons/backToTopButton.png";
+import toTopBtn from "../../assets/icons/backToTopButton.svg";
 
 const ToTopBtn = () => {
   return (
-    <a className={styles.toTopBtn} href="google.com">
+    <a className={styles.toTopBtn} href="#header">
       <img src={toTopBtn} alt="" />
     </a>
   );
--- src/components/WppBtn/WppBtn.jsx
+++ src/components/WppBtn/WppBtn.jsx
@@ -4,29 +4,11 @@ import styles from "./WppBtn.module.css";
 import wpp from "../../assets/icons/whatsappBtn.svg";
 
 const WppBtn = () => {
-  const [teste, setTeste] = React.useState(true);
-
-  function handleScroll() {
-    console.log(window.innerHeight);
-    if (window.innerHeight > 663) {
-      setTeste(false);
-    }
-  }
-
-  React.useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <div>
-      {teste ? (
-        <a className={styles.wppBtn} href="google.com">
-          <img src={wpp} alt="" />
-        </a>
-      ) : null}
+      <a className={styles.wppBtn} href="google.com">
+        <img src={wpp} alt="" />
+      </a>
     </div>
   );
 };

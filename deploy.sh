0ed354d1596ffb10271a5f4a1e90889b20f228bf
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-01-02 08:43:21 -0300

    update

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo "Building app..."
 npm run build
 
 echo "Deploying files to server..."
-scp -r build/* carlos@191.252.186.61:/var/www/191.252.186.61/
+scp -r build/* carlos@191.252.186.61:/var/www/marquesefinelli.com.br/
 
 echo "Done!"
